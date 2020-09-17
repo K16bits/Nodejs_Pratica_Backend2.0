8c7850476c6a986c2e57f891760e9cd3c3c5d34f
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2020-09-17 15:58:43 -0300

    Update 20200902144555_users.js

--- src/database/migrations/20200902144555_users.js
+++ src/database/migrations/20200902144555_users.js
@@ -1,9 +1,9 @@
 exports.up = function(knex) {
     return knex.schema.createTable('users', function(table){
         table.string('id').primary();
-        table.string('nome').notNullable();;
-        table.decimal('numero').notNullable();;
-        table.string('email').notNullable();;
+        table.string('nome').notNullable();
+        table.decimal('numero').notNullable();
+        table.string('email').notNullable();
     }
     )};
 
